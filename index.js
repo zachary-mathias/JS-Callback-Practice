68bb5883f541825b9e984c0b1326e757120ad048
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-03-12 14:45:14 -0400

    added finishing touches for assignment

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+move(character).withArrowKeys(100, 250)
+
+// const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
--- newImage.js
+++ newImage.js
@@ -4,3 +4,9 @@ function newImage(url){
     document.body.append(image)
     return image
 }
+let screen = window.innerHeight/1.70
+let heightOfSky = window.innerHeight-screen
+let heightOfGrass = screen
+
+background('assets/sky.png', 0, screen, window.innerWidth/100, heightOfSky/100)
+background('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
